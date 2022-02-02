7d5b8512b0514780fa2c8c66e12b3116082a4017
Tim Daubenschütz <tim@daubenschuetz.de>
2022-02-02 21:14:30 +0100

    Simplify using structs

--- src/Guestbook.sol
+++ src/Guestbook.sol
@@ -4,94 +4,61 @@
 pragma solidity ^0.8.6;
 
 import {StateTree} from "indexed-sparse-merkle-tree/StateTree.sol";
-import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
-function leaf(
-    string calldata _text,
-    address _owner,
-    uint256 _price,
-    uint256 _blockNumber
-) pure returns (bytes32) {
-    if (_price == 0) {
+function hash(Leaf memory l) pure returns (bytes32) {
+    if (l.price == 0) {
         return 0;
     }
-    return keccak256(abi.encode(_text, _owner, _price, _blockNumber));
+    return keccak256(abi.encode(
+        l.text,
+        l.owner,
+        l.price,
+        l.blockNumber
+    ));
 }
 
-uint256 constant BLOCK_TAX_NUMERATOR = 1;
-uint256 constant BLOCK_TAX_DENOMINATOR = 100;
-
-function tax(
-    uint256 startBlock,
-    uint256 endBlock,
-    uint256 price
-) pure returns (uint256) {
-    uint256 diff = endBlock - startBlock;
-    uint256 scaledDenominator = BLOCK_TAX_DENOMINATOR * FixedPointMathLib.WAD;
-    return FixedPointMathLib.fdiv(
-        price * diff * BLOCK_TAX_NUMERATOR,
-        scaledDenominator,
-        FixedPointMathLib.WAD
-    );
+struct Leaf {
+    string text;
+    address owner;
+    uint256 price;
+    uint256 blockNumber;
 }
 
+struct Proof {
+    bytes32[] list;
+    uint8 bits;
+}
 
 struct Transition {
     uint256 postId;
-    string oldEntry;
-    string newEntry;
-    uint256 oldPrice;
-    address oldOwner;
-    uint256 blockNumber;
+    Leaf prev;
+    Proof proof;
 }
 
 contract Guestbook {
     bytes32 public root;
-    address owner;
-    event Entry(
-        address indexed _from,
-        uint256 indexed _postId,
-        string _entry,
-        uint256 _amount
-    );
 
-    constructor(address _owner) {
+    constructor() {
         root = StateTree.empty();
-        owner = _owner;
     }
 
-    function withdraw() external {
-        require(msg.sender == owner, "only owner");
-        payable(msg.sender).transfer(address(this).balance);
-    }
-
-    function enter(
-        Transition calldata _transition,
-        bytes32[] calldata _proofs,
-        uint8 _bits
-    ) external payable {
-        require(msg.value > 0, "need more than zero");
-        require(msg.value >= _transition.oldPrice, "price too low");
-        root = StateTree.write(
-            _proofs,
-            _bits,
-            _transition.postId,
-            leaf(_transition.newEntry, msg.sender, msg.value, block.number),
-            leaf(_transition.oldEntry, _transition.oldOwner, _transition.oldPrice, _transition.blockNumber),
+    function enter(Transition memory t, string memory text) external payable {
+        Leaf memory next = Leaf(text, msg.sender, msg.value, block.number);
+        bool valid = StateTree.validate(
+            t.proof.list,
+            t.proof.bits,
+            t.postId,
+            hash(t.prev),
             root
         );
-
-        uint256 taxes = tax(_transition.blockNumber, block.number, _transition.oldPrice);
-        uint256 payout = _transition.oldPrice - taxes;
-        if (payout > 0) {
-            payable(_transition.oldOwner).transfer(payout);
-        }
-
-        emit Entry(
-            msg.sender,
-            _transition.postId,
-            _transition.newEntry,
-            msg.value
+        require(valid, "invalid proof");
+        root = StateTree.compute(
+            t.proof.list,
+            t.proof.bits,
+            t.postId,
+            hash(next)
         );
+
+
     }
 }
--- src/Guestbook.t.sol
+++ src/Guestbook.t.sol
@@ -4,7 +4,7 @@
 pragma solidity ^0.8.6;
 import "ds-test/test.sol";
 
-import { Guestbook, Transition, tax, leaf } from "./Guestbook.sol";
+import { Guestbook, Transition, Proof, Leaf, hash } from "./Guestbook.sol";
 import { StateTree } from "indexed-sparse-merkle-tree/StateTree.sol";
 
 abstract contract Hevm {
@@ -18,73 +18,75 @@ contract GuestbookTest is DSTest {
     receive() external payable { }
 
     function setUp() public {
-        g = new Guestbook(address(this));
+        g = new Guestbook();
 		hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
     }
 
-    function testBlockTax() public {
-        assertEq(tax(0, 50, 1 ether), 0.5 ether);
-        assertEq(tax(0, 1, 1 ether), 0.01 ether);
-    }
-
     function testEnterFirst() public {
-       	bytes32[] memory proofs = new bytes32[](0);
-        Transition memory t = Transition(0, "", "hello world", 0, address(this), 0);
-		g.enter{value: 1}(t, proofs, 0);
+        Proof memory p = Proof(new bytes32[](0), 0);
+        Leaf memory l = Leaf("", address(0), 0, 0);
+        uint256 postId = 0;
+        Transition memory t = Transition(postId, l, p);
+		g.enter{value: 1}(t, "hello world");
     }
 
     function failEnteringWithNoValue() public {
-       	bytes32[] memory proofs = new bytes32[](0);
-        Transition memory t = Transition(0, "", "hello world", 0, address(this), 0);
-		g.enter(t, proofs, 0);
+        Proof memory p = Proof(new bytes32[](0), 0);
+        Leaf memory l = Leaf("", address(0), 0, 0);
+        uint256 postId = 0;
+        Transition memory t = Transition(postId, l, p);
+		g.enter(t, "hello world");
     }
 
     function testEnterFirstAndSecond() public {
-       	bytes32[] memory proofs = new bytes32[](0);
-        Transition memory t = Transition(0, "", "hello world", 0, address(this), 0);
+        Proof memory p1 = Proof(new bytes32[](0), 0);
+        Leaf memory l1 = Leaf("", address(0), 0, 0);
+        uint256 postId1 = 0;
+        Transition memory t1 = Transition(postId1, l1, p1);
+        string memory text1 = "hello world";
         uint256 blockNumber = block.number;
-		g.enter{value: 1}(t, proofs, 0);
+		g.enter{value: 1}(t1, text1);
 
-        Transition memory t2 = Transition(1, "", "World", 0, address(this), blockNumber);
-       	bytes32[] memory proofs2 = new bytes32[](1);
-        proofs2[0] = keccak256(abi.encode(t.newEntry, address(this), 1, 0));
-        g.enter{value: 1}(t2, proofs2, StateTree.bitmap(0));
+        Proof memory p2 = Proof(new bytes32[](1), StateTree.bitmap(0));
+        Leaf memory nextL1 = Leaf(text1, address(this), 1, blockNumber);
+        p2.list[0] = hash(nextL1);
+        Leaf memory l2 = Leaf("", address(0), 0, 0);
+        uint256 postId2 = 1;
+        Transition memory t2 = Transition(postId2, l2, p2);
+        string memory text2 = "second entry";
+
+        g.enter{value: 1}(t2, text2);
     }
 
     function testOverbiddingPost() public {
-       	bytes32[] memory proofs = new bytes32[](0);
-        Transition memory t = Transition(0, "", "hello", 0, address(this), 0);
         uint256 blockNumber = block.number;
-		g.enter{value: 1}(t, proofs, 0);
+        Proof memory p1 = Proof(new bytes32[](0), 0);
+        Leaf memory l1 = Leaf("", address(0), 0, 0);
+        uint256 postId1 = 0;
+        Transition memory t1 = Transition(postId1, l1, p1);
+        string memory text1 = "hello world";
+		g.enter{value: 1}(t1, text1);
 
-        Transition memory t2 = Transition(0, t.newEntry, "world", 1, address(this), blockNumber);
-       	bytes32[] memory proofs2 = new bytes32[](0);
-        g.enter{value: 2}(t2, proofs2, 0);
+        Proof memory p2 = Proof(new bytes32[](0), 0);
+        Leaf memory l2 = Leaf(text1, address(this), 1, blockNumber);
+        uint256 postId2 = 0;
+        Transition memory t2 = Transition(postId2, l2, p2);
+        string memory text2 = "second entry";
+
+        g.enter{value: 2}(t2, text2);
     }
 
-    function testSimulatingTaxation() public {
-       	bytes32[] memory proofs = new bytes32[](0);
-        Transition memory t = Transition(0, "", "hello", 0, address(this), 0);
-		hevm.roll(0);
-        uint256 blockNumber = block.number;
-		g.enter{value: 1 ether}(t, proofs, 0);
+    //function testSimulatingTaxation() public {
+    //   	bytes32[] memory proofs = new bytes32[](0);
+    //    Transition memory t = Transition(0, "", "hello", 0, address(this), 0);
+	//	hevm.roll(0);
+    //    uint256 blockNumber = block.number;
+	//	g.enter{value: 1 ether}(t, proofs, 0);
 
-        Transition memory t2 = Transition(0, t.newEntry, "world", 1 ether, address(this), blockNumber);
-       	bytes32[] memory proofs2 = new bytes32[](0);
-		hevm.roll(50);
-        g.enter{value: 2 ether}(t2, proofs2, 0);
-		assertEq(address(g).balance, 2.5 ether);
-    }
-
-    function testWithdrawing() public {
-       	bytes32[] memory proofs = new bytes32[](0);
-        Transition memory t = Transition(0, "", "hello world", 0, address(this), 0);
-		g.enter{value: 1 ether}(t, proofs, 0);
-
-        uint256 balance = address(g).balance;
-        assertEq(balance, 1 ether);
-        g.withdraw();
-        uint256 balance2 = address(g).balance;
-        assertEq(balance2, 0 ether);
-    }
+    //    Transition memory t2 = Transition(0, t.newEntry, "world", 1 ether, address(this), blockNumber);
+    //   	bytes32[] memory proofs2 = new bytes32[](0);
+	//	hevm.roll(50);
+    //    g.enter{value: 2 ether}(t2, proofs2, 0);
+	//	assertEq(address(g).balance, 2.5 ether);
+    //}
 }
